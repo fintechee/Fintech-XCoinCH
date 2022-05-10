d15f956ad00ce90cd2c552c17be628c06a214c0c
Fintechee <admin@fintechee.com>
2022-05-10 22:05:28 +0800

    Add files via upload

--- js/cc/builtin.js
+++ js/cc/builtin.js
@@ -115,6 +115,34 @@ function lwma (dataInput, dataOutput, calculatedLength, period) {
 }
 
 function importBuiltInIndicators () {
+	importBuiltInIndicator("volume", "Volume of OHLC(v1.0)", function (context) {
+		var dataInput = getDataInput(context, 0)
+		var dataOutput = getDataOutput(context, "volume")
+
+		var calculatedLength = getCalculatedLength(context)
+		var i = calculatedLength
+
+		if (i != 0) {
+			i--
+		}
+
+		while (i < dataInput.length) {
+			dataOutput[i] = dataInput[i]
+			i++
+		}
+	},[],
+	[{
+		name: DATA_NAME.VOLUME,
+		index: 0
+	}],
+	[{
+		name: "volume",
+		visible: true,
+		renderType: RENDER_TYPE.HISTOGRAM,
+		color: "steelblue"
+	}],
+	WHERE_TO_RENDER.SEPARATE_WINDOW)
+
 	importBuiltInIndicator("sma", "Simple Moving Average(v1.0)", function (context) {
 		var dataInput = getDataInput(context, 0)
 		var dataOutput = getDataOutput(context, "sma")
@@ -3669,6 +3697,47 @@ function importBuiltInIndicators () {
 	    visible: true,
 	    renderType: RENDER_TYPE.LINE,
 	    color: "#ECAE93"
+	}],
+	WHERE_TO_RENDER.SEPARATE_WINDOW)
+
+	importBuiltInIndicator("mfi", "Market Facilitation Index(v1.0)", function (context) {
+		var dataInputHigh = getDataInput(context, 0)
+		var dataInputLow = getDataInput(context, 1)
+		var dataInputVol = getDataInput(context, 2)
+		var dataOutput = getDataOutput(context, "mfi")
+
+		var calculatedLength = getCalculatedLength(context)
+		var i = calculatedLength
+
+		if (i != 0) {
+			i--
+		}
+
+		while (i < dataInputVol.length) {
+			if (dataInputVol[i] == 0) {
+				dataOutput[i] = 0
+			} else {
+				dataOutput[i] = (dataInputHigh[i] - dataInputLow[i]) / dataInputVol[i]
+			}
+
+			i++
+		}
+	},[],
+	[{
+    name: DATA_NAME.HIGH,
+    index: 0
+	},{
+    name: DATA_NAME.LOW,
+    index: 1
+	},{
+		name: DATA_NAME.VOLUME,
+		index: 2
+	}],
+	[{
+		name: "mfi",
+		visible: true,
+		renderType: RENDER_TYPE.HISTOGRAM,
+		color: "steelblue"
 	}],
 	WHERE_TO_RENDER.SEPARATE_WINDOW)
 
